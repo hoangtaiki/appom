f0e0497fd5f7c7992c14d00e67cb506ce02a07c9
Harry.Tran <hoang@platphormcorp.com>
2018-10-26 19:26:23 +0700

    Implement Wait class and some helper functions

--- lib/appom/element_container.rb
+++ lib/appom/element_container.rb
@@ -36,7 +36,7 @@ module Appom
       #     element :email, :accessibility_id, 'email_text_field'
       #
       # @param name             Element name
-      # @param *find_args       An array contain information to find the element. It contains locator stratery and search target
+      # @param *find_args       An array contain information to find the element. It contains locator strategy and search target
       # http://appium.io/docs/en/commands/element/find-element/
       #
       # Element doesn't support block so that will raise if pass a block when declare
@@ -57,7 +57,7 @@ module Appom
       #     elements :contact_cell, :accessibility_id, 'contact_cell'
       #
       # @param name             Element name
-      # @param *find_args       An array contain information to find the elements. It contains locator stratery and search target
+      # @param *find_args       An array contain information to find the elements. It contains locator strategy and search target
       # http://appium.io/docs/en/commands/element/find-element/
       #
       # Elements doesn't support block so that will raise if pass a block when declare
@@ -83,7 +83,7 @@ module Appom
 
       private
 
-      # Add item to @mapped_items or define mothod to notify that we can't find item without args
+      # Add item to @mapped_items or define method to notify that we can't find item without args
       def build(name, *find_args)
         if find_args.empty?
           create_error_method(name)
@@ -91,12 +91,70 @@ module Appom
           add_to_mapped_items(name)
           yield
         end
+        add_helper_methods(name, *find_args)
       end
 
-      # Define mothod to notify that we can't find item without args
+      # Define method to notify that we can't find item without args
       def create_error_method(name)
         define_method(name) do
           raise Appom::InvalidElementError
+        end
+      end
+
+      def add_helper_methods(name, *find_args)
+        create_existence_checker(name, *find_args)
+        create_nonexistence_checker(name, *find_args)
+        create_get_all_elements(name, *find_args)
+      end
+
+      def create_helper_method(proposed_method_name, *find_args)
+        if find_args.empty?
+          create_error_method(proposed_method_name)
+        else
+          yield
+        end
+      end
+
+      ##
+      # Check element exist
+      # We will try to find all elements with *find_args
+      # Condition is pass when response is not empty
+      #
+      def create_existence_checker(element_name, *find_args)
+        method_name = "has_#{element_name}?"
+        create_helper_method(method_name, *find_args) do
+          define_method(method_name) do |*runtime_args|
+            args = merge_args(find_args, runtime_args)
+            wait_check_util_not_empty(*args)
+          end
+        end
+      end
+
+      ##
+      # Check element non-existent
+      # We will try to find all elements with *find_args
+      # Condition is pass when response is empty
+      #
+      def create_nonexistence_checker(element_name, *find_args)
+        method_name = "has_no_#{element_name}?"
+        create_helper_method(method_name, *find_args) do
+          define_method(method_name) do |*runtime_args|
+            args = merge_args(find_args, runtime_args)
+            wait_check_util_empty(*args)
+          end
+        end
+      end
+
+      ##
+      # Try to get all elements until not get empty array
+      #
+      def create_get_all_elements(element_name, *find_args)
+        method_name = "get_all_#{element_name}"
+        create_helper_method(method_name, *find_args) do
+          define_method(method_name) do |*runtime_args|
+            args = merge_args(find_args, runtime_args)
+            wait_check_util_empty(*args)
+          end
         end
       end
     end
--- lib/appom/page.rb
+++ lib/appom/page.rb
@@ -5,12 +5,52 @@ module Appom
 
     # Find an element
     def find(*find_args)
-      Appom.driver.find_element(*find_args)
+      wait = Wait.new(timeout: Appom.max_wait_time)
+      wait.until { Appom.driver.find_element(*find_args) }
     end
 
     # Find elements
     def all(*find_args)
       Appom.driver.find_elements(*find_args)
+    end
+
+    ##
+    # Use wait to check element non-exist
+    # Before timeout we will try to find elements and check response is empty
+    #
+    def wait_check_util_empty(*find_args)
+      wait = Wait.new(timeout: Appom.max_wait_time)
+      wait.until do
+        Appom.driver.find_elements(*find_args).empty?
+      end
+    end
+
+    ##
+    # Use wait to check element exist
+    # Before timeout we will try to find elements and check response is not empty
+    #
+    def wait_check_util_not_empty(*find_args)
+      wait = Wait.new(timeout: Appom.max_wait_time)
+      wait.until do
+        !Appom.driver.find_elements(*find_args).empty?
+      end
+    end
+
+    ##
+    # Use wait to get elements
+    # Before timeout we will try to find elements until response is not empty
+    #
+    def wait_util_get_not_empty(*find_args)
+      wait = Wait.new(timeout: Appom.max_wait_time)
+      wait.until do
+        result = Appom.driver.find_elements(*find_args)
+        # If reponse is empty we will return false to make it not pass Wait condition
+        if result.empty?
+          return false
+        end
+        # Return result
+        return result
+      end
     end
   end
 end
