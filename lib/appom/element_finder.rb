5db7573abc631d10eb5e37490384207f4ec0bce3
Harry Tran <hoang@platphormcorp.com>
2019-03-26 20:27:46 +0700

    Remove used functions

--- lib/appom/element_finder.rb
+++ lib/appom/element_finder.rb
@@ -10,52 +10,7 @@ module Appom
     def _all(*find_args)
       page.find_elements(*find_args)
     end
-
-    ##
-    # Use wait to check element non-exist
-    # Before timeout we will try to find elements and check response is empty
-    #
-    def wait_check_until_empty(*find_args)
-      wait = Wait.new(timeout: Appom.max_wait_time)
-      wait.until do
-        page.find_elements(*find_args).empty?
-      end
-    end
-
-    ##
-    # Use wait to check element exist
-    # Before timeout we will try to find elements and check response is not empty
-    #
-    def wait_check_until_not_empty(*find_args)
-      wait = Wait.new(timeout: Appom.max_wait_time)
-      wait.until do
-        !page.find_elements(*find_args).empty?
-      end
-    end
-
-
-
-    ##
-    # Wait until an element will be enable
-    #
-    def wait_until_element_enabled(*find_args)
-      wait = Wait.new(timeout: Appom.max_wait_time)
-      wait.until { page.find_element(*find_args).enabled? }
-    end
-
-    ##
-    # Wait until an element will be disable
-    #
-    def wait_until_element_disabled(*find_args)
-      wait = Wait.new(timeout: Appom.max_wait_time)
-      wait.until do  !page.
-          find_element(*find_args).enabled?
-      end
-    end
-
-
-
-
+    
     ##
     # Use wait to get elements
     # Before timeout we will try to find elements until response return array is not empty
